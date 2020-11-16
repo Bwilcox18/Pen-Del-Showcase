d78f03ca3c25ca410ba5693b420ca1a6a4bfaec0
Brendon Wilcox <mrw321@gmail.com>
2020-11-15 21:28:07 -0500

    Corrected before and after pics

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -1,15 +1,46 @@
+
 import React, { Component }  from 'react';
 
 class Main extends Component {
     render() {
         return(
             <div className="container">
-                <div className="row">
+                <div className="row py-3">
+                    <div className="col-6" >
+                        <h1>
+                            Before
+                        </h1>
+                    </div>
+                    <div className="col-6" >
+                        <h1>
+                            After
+                        </h1>
+                    </div>
+                </div>
+                <div className="row pb-3">
                     <div class="col-6">
-                        <img src="/assets/images/7Eleven.JPEG" alt="" className="img-fluid" />
+                        <img src="/assets/images/bai.jpg" alt="" width="400" className="rounded img-thumbnail" />
                     </div>
                     <div class="col-6">
-                        <img src="/assets/images/logo.png" alt="" class="img-fluid rounded" />
+                            <img src="/assets/images/Door.jpg" alt="" width="400" className="rounded img-thumbnail" />
+                    </div>
+                </div>
+                <hr />
+                <div className="row py-3">
+                    <div class="col-6">
+                        <img src="/assets/images/gatorade.jpg" alt="" width="400" className="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-6">
+                            <img src="/assets/images/milk.jpg" alt="" width="400" className="rounded img-thumbnail" />
+                    </div>
+                </div>
+                <hr />
+                <div className="row py-3">
+                    <div class="col-6">
+                        <img src="/assets/images/soda.jpg" alt="" width="400" className="rounded img-thumbnail" />
+                    </div>
+                    <div class="col-6">
+                            <img src="/assets/images/water.jpg" alt="" width="400" className="rounded img-thumbnail" />
                     </div>
                 </div>
             </div>
